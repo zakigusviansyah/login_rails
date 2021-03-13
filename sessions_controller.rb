a80cb3e9ddca1779cde1741d995b1b481da742e4
zakigusviansyah <62235094+zakigusviansyah@users.noreply.github.com>
2021-03-13 11:22:19 +0700

    Add files via upload

---
+++ sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+        
+    end
+
+    def create
+        username = params[:username]
+        password = params[:password]
+    
+        user = User.find_by(username: username)
+        if user
+            if user.authenticate(password)
+                redirect_to books_path, notice: 'Anda telah login'
+            else
+                redirect_to new_session_path, notice: 'Username atau password anda salah'
+            end
+        else
+            redirect_to new_session_path, notice: 'Username atau password anda salah'
+        end
+    end
+end
